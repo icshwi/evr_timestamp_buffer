1a11440aaa33663989adba2b34a80e576f6d08e0
Nicklas Holmberg <nicklas.holmberg@esss.se>
2018-12-20 15:43:38 +0100

    64-bit integer development

--- evr_timestamp_bufferApp/src/nano_sec_timestamp.c
+++ evr_timestamp_bufferApp/src/nano_sec_timestamp.c
@@ -15,15 +15,22 @@ static int nano_second_timestamp(aSubRecord *prec) {
 
     if(!dbGetTimeStamp(&(prec->inpa),&tick)){
 	/* Add constant (20*365.25*24*3600=631152000) to change from Epics epoch to Unix epoch */
-        *(long long *)prec->vala =  (((long long) tick.secPastEpoch) + 631152000) * 1000000000 + (long long) tick.nsec;
-//	printf("Timestamp: %lld \n", *(long long *)prec->vala);
+        *(epicsInt64 *)prec->vala =  (((epicsInt64) tick.secPastEpoch) + 631152000) * 1000000000 + (epicsInt64) tick.nsec;
+        //*(long long *)prec->vala =  (((long long) tick.secPastEpoch) + 631152000) * 1000000000 + (long long) tick.nsec;
 	*(long *)prec->valb = tick.secPastEpoch;
         *(long *)prec->valc = tick.nsec;
+//	printf("Timestamp: %lld \n", *(long long *)prec->vala);
         return 0;
     } else {
         printf("Could not retrieve timestamp \n");
         return -1;
     }
+    /*#ifdef DBR_INT64
+        printf("Code where Base has INT64 support");
+    #else
+        printf("Code for older versions");
+    #endif*/
+
 }
  
 /* Note the function must be registered at the end!*/
