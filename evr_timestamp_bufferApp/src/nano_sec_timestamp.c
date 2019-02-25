cd2271742ff3104a2ae56bb9b6565113b0e97f72
Nicklas Holmberg <nicklas.holmberg@esss.se>
2019-02-25 13:37:14 +0100

    Test of int64 in asub

--- evr_timestamp_bufferApp/src/nano_sec_timestamp.c
+++ evr_timestamp_bufferApp/src/nano_sec_timestamp.c
@@ -16,9 +16,13 @@ static int nano_second_timestamp(aSubRecord *prec) {
     if(!dbGetTimeStamp(&(prec->inpa),&tick)){
 	/* Add constant (20*365.25*24*3600=631152000) to change from Epics epoch to Unix epoch */
         *(epicsInt64 *)prec->vala =  (((epicsInt64) tick.secPastEpoch) + 631152000) * 1000000000 + (epicsInt64) tick.nsec;
-        //*(long long *)prec->vala =  (((long long) tick.secPastEpoch) + 631152000) * 1000000000 + (long long) tick.nsec;
-	*(long *)prec->valb = tick.secPastEpoch;
+        *(long long *)prec->valb =  (((long long) tick.secPastEpoch) + 631152000) * 1000000000 + (long long) tick.nsec;
+//	*(long *)prec->valb = tick.secPastEpoch;
         *(long *)prec->valc = tick.nsec;
+        *(epicsInt64 *)prec->vald =  (((epicsInt64) tick.secPastEpoch)) * 1000000000 + (epicsInt64) tick.nsec;
+        *(epicsInt64 *)prec->vale = 9007199254740992; // 1e18 + tick.nsec;
+        *(epicsInt64 *)prec->valf = (epicsInt64) 18014398509481984+3; // 1e18 + tick.nsec;
+        *(epicsUInt64 *)prec->valg =  (((epicsUInt64) tick.secPastEpoch) + 631152000) * 1000000000 + (epicsUInt64) tick.nsec;
 //	printf("Timestamp: %lld \n", *(long long *)prec->vala);
         return 0;
     } else {
