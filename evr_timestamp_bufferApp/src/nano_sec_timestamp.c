b0156004f3cf40ab5d91d5fd74f14ca8a3881265
Douglas Bezerra Beniz <douglas.bezerra.beniz@esss.se>
2019-03-19 15:57:39 +0100

    version with two timestamps Unix and Epics

--- evr_timestamp_bufferApp/src/nano_sec_timestamp.c
+++ evr_timestamp_bufferApp/src/nano_sec_timestamp.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <aSubRecord.h>
 #include <registryFunction.h>
 #include <epicsExport.h>
@@ -21,7 +22,7 @@ static int nano_second_timestamp(aSubRecord *prec) {
     gettimeofday(&te, NULL); // get current time
     unsigned long time_in_sec = te.tv_sec; 
     unsigned long time_in_usec = te.tv_usec;
-    
+    char s[25]; 
 
     if(!dbGetTimeStamp(&(prec->inpa),&tick)){
 	/* Add constant (20*365.25*24*3600=631152000) to change from Epics epoch to Unix epoch */
@@ -31,10 +32,12 @@ static int nano_second_timestamp(aSubRecord *prec) {
 	*(long *)prec->valc = tick.secPastEpoch; //Epics epoch
 	*(long *)prec->vald = time_in_sec; 
 	*(long *)prec->vale = time_in_usec; 
-	*(long *)prec->valf = (tick.secPastEpoch + 631152000 - time_in_sec) + (tick.nsec/1000L - time_in_usec)/1000000L; 
+	*(long *)prec->valf = (tick.secPastEpoch + 631152000 - time_in_sec) * 1000000L + (tick.nsec/1000L - time_in_usec); 
+        strcpy(s, (char*)prec->b);
         //*(epicsInt64 *)prec->vald =  (((epicsInt64) tick.secPastEpoch) + 631152000) * 1000000000 + (epicsInt64) tick.nsec;
-	printf("TEpics[sec]: %lld,\t [usec]: %lld\n", *(long long *)prec->vala,  *(long long *)prec->valb);
-	printf("TUnix[sec] : %lld,\t [usec]: %lld,\t dt:%lld\n", *(long long *)prec->vald,  *(long long *)prec->vale,  *(long long *)prec->valf);
+        //printf("inpb       : %s\n", s);
+	//printf("TEpics[sec]: %lld,\t [usec]: %lld\n", *(long long *)prec->vala,  *(long long *)prec->valb/1000L);
+	//printf("TUnix[sec] : %lld,\t [usec]: %lld,\t dt:%lld\n\n", *(long long *)prec->vald,  *(long long *)prec->vale,  *(long long *)prec->valf);
         return 0;
     } else {
         printf("Could not retrieve timestamp \n");
